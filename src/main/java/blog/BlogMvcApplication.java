0c068fc8c0ac8ea942f6598258bf9aee59ec1d29
Kyle Bustard <bustardkyle@gmail.com>
2017-03-16 23:53:52 -0500

    initial commit

---
+++ src/main/java/blog/BlogMvcApplication.java
@@ -0,0 +1,15 @@
+package blog;
+
+/**
+ * Created by kylebustard on 3/16/17.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMvcApplication.class, args);
+    }
+}
---
+++ src/main/java/blog/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package blog.controllers;
+
+/**
+ * Created by kylebustard on 3/16/17.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
